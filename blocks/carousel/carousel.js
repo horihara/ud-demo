200a1dccde6400d98d486ca281467f4c2e49c538
horihara <horihara@adobe.com>
2025-01-27 13:33:47 +0900

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -5,26 +5,26 @@ const placeholders = await fetchPlaceholders(getMetadata('locale'));
 const { btnNxt, btnPre } = placeholders;
 export default function decorate(block) {
   console.log('placeholders ---> ', placeholders, btnNxt, btnPre);
-  const rows= [...block.children];
+  const rows = [...block.children];
   [...block.children].forEach((row, r) => {
-    if(r==0){
+    if (r==0) {
       const nextbtn = document.createElement('button');
       nextbtn.classList.add('btn');
       nextbtn.classList.add('btn-next');
       const node = document.createTextNode(btnNxt);
       nextbtn.append(node);
       row.replaceWith(nextbtn);
-    }else if(r==rows.length-1){
+    } else if (r==rows.length-1){
       const prebtn = document.createElement('button');
       prebtn.classList.add('btn');
       prebtn.classList.add('btn-prev');
       const node = document.createTextNode(btnPre);
       prebtn.append(node);
       row.replaceWith(prebtn);
-    }else{
+    } else {
       row.classList.add('slide');
       [...row.children].forEach((col, c) => {
-        if(c==1){
+        if (c==1){
           col.classList.add('slide-text');
         }
       });
